afd8c62e6efa6339484090ef3b06769b38a3ca8f
ida-cy <ida.caspary@ibm.com>
2022-11-02 18:21:04 +0000

    end of practical 2

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -15,6 +15,11 @@ import triangle.syntacticAnalyzer.SourceFile;
 public class TestScanner {
 
 	@Test
+	public void testAdd() {
+		compileExpectSuccess("/add.tri");
+	}
+
+	@Test
 	public void testHi() {
 		compileExpectSuccess("/hi.tri");
 	}
@@ -22,25 +27,25 @@ public class TestScanner {
 
 	@Test
 	public void testHiNewComment() {
-		compileExpectFailure("/hi-newcomment.tri");
+		compileExpectSuccess("/hi-newcomment.tri");
 	}
 	
 
 	@Test
 	public void testHiNewComment2() {
-		compileExpectFailure("/hi-newcomment2.tri");
+		compileExpectSuccess("/hi-newcomment2.tri");
 	}
 	
 
 	@Test
 	public void testBarDemo() {
-		compileExpectFailure("/bardemo.tri");
+		compileExpectSuccess("/bardemo.tri");
 	}
 	
 
 	@Test
 	public void testRepeatUntil() {
-		compileExpectFailure("/repeatuntil.tri");
+		compileExpectSuccess("/repeatuntil.tri");
 	}
 	
 	
